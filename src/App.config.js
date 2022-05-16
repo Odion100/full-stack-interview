61ac783d5dc0abc39419198da3202ffd30f6a86f
odion100 <odionedwards@gmail.com>
2022-05-16 07:20:27 -0400

    created app.congig file

---
+++ src/App.config.js
@@ -0,0 +1,47 @@
+const attacks = [
+  {
+    name: "Super Nova Blast",
+    points: 150,
+    color: "red",
+  },
+  {
+    name: "Solar Wind Flare",
+    points: 300,
+    color: "red",
+  },
+  {
+    name: "Gama Ray Beam",
+    points: 600,
+    color: "red",
+  },
+  {
+    name: "Gravity Wave",
+    points: 150,
+    color: "black",
+  },
+  {
+    name: "Dark Matter Effect",
+    points: 300,
+    color: "black",
+  },
+  {
+    name: "Black Whole Void",
+    points: 600,
+    color: "black",
+  },
+  {
+    name: "Metero Shower",
+    points: 150,
+    color: "blue",
+  },
+  {
+    name: "Grav",
+    points: 300,
+    color: "blue",
+  },
+  {
+    name: "Planetary Destruction",
+    points: 600,
+    color: "blue",
+  },
+];
