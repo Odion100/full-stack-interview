ec5472511d3f6c79b38e939552300f961ae2cbaa
odion100 <odionedwards@gmail.com>
2022-05-16 06:44:27 -0400

    fetching table data and adding table row

--- src/organisms/RobotsTable/RobotsTable.js
+++ src/organisms/RobotsTable/RobotsTable.js
@@ -1,16 +1,37 @@
 import React, { useState, useEffect } from "react";
+import request from "request";
+import Textbox from "../../atoms/Textbox/Textbox";
+import Button from "../../atoms/Botton/Button";
 import "./styles.scss";
 
-const RobotsTable = ({ data, submit }) => {
+const RobotsTable = () => {
+  const [robot_data, updateRobotData] = useState([]);
+
+  const getRobots = () => {
+    request({ url: "http://localhost:3001/robots" }, (err, res, body) => {
+      if (err) console.log(err);
+      else if (res.statusCode >= 400) console.log(body);
+      else updateRobotData(body.robot_data || []);
+      console.log(body);
+    });
+  };
+  const addRow = () => {
+    const data = [{ edit_mode: true, attacks: [] }, ...robot_data];
+    updateRobotData(data);
+  };
+
+  useEffect(() => {
+    getRobots();
+  }, []);
+
   const headers = [
     { name: "Name" },
     { name: "Color" },
     { name: "Attack" },
     { name: "Defence" },
-    { name: "" },
-    { name: "" },
+    { name: <Button submit={addRow}>Add Robot</Button> },
   ];
-
+  console.log("robot data", robot_data);
   return (
     <table className={`data-table`}>
       <thead>
@@ -21,16 +42,40 @@ const RobotsTable = ({ data, submit }) => {
         </tr>
       </thead>
       <tbody>
-        {/* {table.map((data, i) => (
-          <tr key={i}>
-            {data.map((value, i) => (
-              <td key={i}>{value}</td>
-            ))}
-          </tr>
-        ))} */}
+        {robot_data.map((robot, i) =>
+          robot.edit_mode ? <TableRowEditor {...robot} key={i} /> : <TableRow {...robot} key={i} />
+        )}
       </tbody>
     </table>
   );
 };
 
+const TableRow = ({ name, color, attacks, defence, image, i }) => (
+  <tr key={i}>
+    <td>{name}</td>
+    <td>{color}</td>
+    <td>
+      {attacks.map(({ name, points }, i) => (
+        <div key={i}>
+          <span>{name}</span>:<span>{points}</span>
+        </div>
+      ))}
+    </td>
+    <td>{defence}</td>
+    <td>
+      <img src={image} alt="Robot" />
+    </td>
+  </tr>
+);
+const TableRowEditor = ({ name, color, attack, defence, image, i }) => {
+  return (
+    <tr key={i}>
+      <td>{<Textbox />}</td>
+      <td>{color}</td>
+      <td>{attack}</td>
+      <td>{defence}</td>
+      <td>{image}</td>
+    </tr>
+  );
+};
 export default RobotsTable;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,18 +4,6 @@ import RobotsTable from "../../organisms/RobotsTable/RobotsTable";
 import request from "request";
 
 const Home = (props) => {
-  const [robot_data, updateRobotData] = useState([]);
-
-  useEffect(() => {
-    console.log("1234--------");
-    request({ url: "http://localhost:3001/robots" }, (err, res, body) => {
-      if (err) console.log(err);
-      else if (res.statusCode >= 400) console.log(body);
-      else updateRobotData(body.robot_data);
-      console.log(body);
-    });
-  }, []);
-
   return (
     <section className="home">
       <div className="container">
