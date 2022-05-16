85935cf5726954cb58e12aafca026b57276adf10
odion100 <odionedwards@gmail.com>
2022-05-16 14:12:42 -0400

    added route to battle page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-
+import BattleArena from "./pages/BattleArena/BattleArena";
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="battle-arena/:robot1/:robot2" element={<BattleArena />} />
         </Routes>
       </Router>
     </div>
