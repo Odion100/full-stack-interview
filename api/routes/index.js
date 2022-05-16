da3601bf9ca4c83e5354656a1cc03cd9d457df32
odion100 <odionedwards@gmail.com>
2022-05-16 15:55:33 -0400

    added battles routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const Robots = require("./Robots/Robots")();
+const Battles = require("./Battles/Battles")();
 const url = `mongodb+srv://odion:${process.env.MONGODB_PASSWORD}@cluster0.oehof.mongodb.net/battle-ship-space?retryWrites=true&w=majority`;
 mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -40,5 +41,26 @@ router.delete("/robots", function (req, res, next) {
       res.status(error.status).json(error);
     } else res.json(results);
   });
+});
+
+router.get("/battles", function (req, res, next) {
+  const { query } = req;
+  Battles.find(query, (error, results) => {
+    if (error) {
+      error.status = error.status || 500;
+      res.status(error.status).json(error);
+    } else res.json(results);
+  });
+});
+router.put("/battles", function (req, res, next) {
+  const { body } = req;
+  const cb = (error, results) => {
+    if (error) {
+      error.status = error.status || 500;
+      res.status(error.status).json(error);
+    } else res.json(results);
+  };
+
+  Battles.add(body, cb);
 });
 module.exports = router;
