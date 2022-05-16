d336b0b2eaddbf78ca61c8d633dad13c72b194d4
odion100 <odionedwards@gmail.com>
2022-05-15 21:45:00 -0400

    fetching robot data to the frontend

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.scss";
 import RobotsTable from "../../organisms/RobotsTable/RobotsTable";
+import request from "request";
 
 const Home = (props) => {
+  const [robot_data, updateRobotData] = useState([]);
+
+  useEffect(() => {
+    console.log("1234--------");
+    request({ url: "http://localhost:3001/robots" }, (err, res, body) => {
+      if (err) console.log(err);
+      else if (res.statusCode >= 400) console.log(body);
+      else updateRobotData(body.robot_data);
+      console.log(body);
+    });
+  }, []);
+
   return (
     <section className="home">
       <div className="container">
