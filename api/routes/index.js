07075edd2fa9d41b3411a209c2023862a96604f7
odion100 <odionedwards@gmail.com>
2022-05-17 18:34:12 -0400

    code clean up

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const Robots = require("./Robots/Robots")();
-const Battles = require("./Battles/Battles")();
 const url = `mongodb+srv://odion:${process.env.MONGODB_PASSWORD}@cluster0.oehof.mongodb.net/battle-ship-space?retryWrites=true&w=majority`;
 mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((data) => console.log("mongodb connected:-->"))
   .catch((err) => console.log("mongodb connection failed:-->", err));
 
+const Robots = require("./Robots/Robots")();
+const Battles = require("./Battles/Battles")();
 const express = require("express");
 const router = express.Router();
 const cb = (req, res, error, results) => {
